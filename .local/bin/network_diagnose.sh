c8ec58361947b9b055c8f8869a39e5674b75464d
Jakub Parapura <jakubparapura@gmail.com>
2021-09-10 11:33:13 +0200

    added network_diagnose.sh

---
+++ .local/bin/network_diagnose.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Trying to ping localhost."
+ping -c 4 localhost &> /dev/null
+[ $? -ne 0 ] && echo "Failed." && exit 1
+echo "Success."
+
+echo "Trying to ping public IP address."
+ping -c 4 8.8.8.8 &> /dev/null
+[ $? -ne 0 ] && echo "Failed." && exit 1
+echo "Success."
+
+echo "Trying to resolve domain names."
+ping -c 4 8.8.8.8 &> /dev/null
+[ $? -ne 0 ] && echo "Failed." && exit 1
+echo "Success."
+
+echo "Network configuration is correct."
