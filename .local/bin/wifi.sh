57f69d5d1227e9d19356453ffcc726393b6f3d99
Jakub Parapura <jakubparapura@gmail.com>
2021-10-03 21:01:53 +0200

    added support for connecting using wpa_supplicant to connect to UJ_WiFi

--- .local/bin/wifi.sh
+++ .local/bin/wifi.sh
@@ -4,6 +4,8 @@
 #!/bin/bash
 
 name=""
+helper="netctl"
+interface="wlp3s0"
 
 if [ $# -ne 1 ]; then
     echo "usage: $0 <ssid alias>"
@@ -15,13 +17,33 @@ case "$1" in
     "gosia") name="ZTE_589D20";;
     "css2") name="EXT_CSS2";;
     "phone") name="iPhone-hotspot";;
+	"uj")
+		helper="wpa_supplicant"
+		name="uj_wifi.conf"
+	;;
     *) name="$1";;
 esac
 
-sudo ip link set wlp3s0 down
-cd "/etc/netctl"
-sudo netctl stop "$name"
-sudo netctl start "$name"
-cd "$HOME"
+case "$helper" in
+	"netctl")
+		sudo ip link set "$interface" down
+		cd "/etc/netctl"
+		sudo netctl stop "$name"
+		sudo netctl start "$name"
+		# TODO consider changing to cd -
+		cd "$HOME"
+	;;
+	"wpa_supplicant")
+		cd "/etc/wpa_supplicant"
+		doas killall wpa_supplicant
+		doas wpa_supplicant -i "$interface" -c "$name"
+		cd -
+	;;
+	*)
+		echo "Unknown helper!"
+		exit 1
+	;;
+esac
+
 refbar.sh
 exit 0
