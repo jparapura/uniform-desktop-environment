d356502e1770d0452267ce1c01a241e3cb2cd16a
Jakub Parapura <jakubparapura@gmail.com>
2021-10-31 21:30:35 +0100

    added option to enable both primary and secondary screen

--- .local/bin/dmenu_monitor.sh
+++ .local/bin/dmenu_monitor.sh
@@ -12,14 +12,14 @@
 primary="LVDS-1"
 secondary="DP-2"
 
-action="$(printf "P+S-\nP-S+" | dmenu -i -p 'Monitor config')"
+action="$(printf "P+S-\nP-S+\nP+S+" | dmenu -i -p 'Monitor config')"
 #action="$(printf "P+S-\nP-S+\nP+S+\nDuplicate" | dmenu -i -p 'Monitor config')"
 [ -n "$action" ] || exit 1
 
 case "$action" in
     "P+S-") 	 xrandr --output "$primary" --auto --output "$secondary" --off;;
 	"P-S+") 	 xrandr --output "$primary" --off  --output "$secondary" --mode 1920x1080;;
-    #"P+S+") 	 xrandr --output "$primary" --auto --output "$secondary" --mode 1920x1080;;
+    "P+S+") 	 xrandr --output "$primary" --auto --output "$secondary" --mode 1920x1080;;
     #"Duplicate") xrandr --output "$primary" --auto --output "$secondary" --mode 1920x1080 --output "$secondary" --same-as "$primary"
 esac
 
